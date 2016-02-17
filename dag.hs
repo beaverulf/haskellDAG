065a185de6acceef9c647aef8077823ce05bcd60
Jonas Nyman <c13jnn@cs.umu.se>
2016-02-17 15:49:47 +0100

    fittzigenare

--- dag.hs
+++ dag.hs
@@ -1,8 +1,20 @@
-module Vertex
-( weight
-,
-, cubeVolume
-, cubeArea
-, cuboidArea
-, cuboidVolume
-) where
+-- Programspråk (5DV086)
+-- Laboration 2
+--
+--
+-- Authors:   Jonas Nyman, c13jnn  
+--			  Björn Lidell, c13bll	
+
+data Vertex = Vertex { weight :: Atom  
+                     } deriving (Show)
+
+
+data Edge = Edge { weight :: Atom
+				   ,origin :: Vertex
+				   ,destination :: Vertex
+				 } deriving (Show)
+
+
+ add_vertex :: a -> b
+
+
